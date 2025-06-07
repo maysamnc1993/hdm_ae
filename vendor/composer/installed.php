4812c0424b6cf6dc4d3b976131d43ba1778d5c9d
Meysam Khatami <khatami.maysam@gmail.com>
2025-06-07 14:51:11 +0330

    Rebuild

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jaber/jthem',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2894c88afde0b554d1226de02dfee829d520f929',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jaber/jthem' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2894c88afde0b554d1226de02dfee829d520f929',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
