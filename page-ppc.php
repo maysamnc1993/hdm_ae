316885e05616c3fda0eed64d098378573e8ac50c
Meysam Khatami <khatami.maysam@gmail.com>
2025-07-22 14:26:12 +0330

    Solved Build

--- page-ppc.php
+++ page-ppc.php
@@ -57,64 +57,67 @@ get_header();
             </div>
 
         </div>
-        
+
+        <div class="ListOfValue" style="background-image:url(http://localhost/HDM-AE/wp-content/uploads/2025/07/fabio-oyXis2kALVg-unsplash1.jpg)">
+        <ul class="list_of_values">
+
+            <li>
+                <h3>Drive Targeted Traffic</h3>
+                <p> Reach people actively searching for your products or services.</p>
+            </li>
+
+            <li>
+                <h3>Maximize Conversion</h3>
+                <p> Convert visitors into real customers by optimizing every click and landing page.</p>
+            </li>
+         
+            <li>
+                <h3>Achieve Better ROI</h3>
+                <p>Get faster results and pay only for clicks that bring real value.</p>
+            </li>
+   <li class="title_box_section">
+            <div class="value_section">
+
+                <div class="title_box">
+                        <h2 style="background-image:url(http://localhost/HDM-AE/wp-content/uploads/2025/05/cta3.jpg)" class="title">Turns Clicks Into Customers with PPC Advertising</h2>
+                        <p class="description">Convert visitors into real customers and boost your ROI with high-performance PPC advertising.</p>
+                        <a href="#" class="CTA_Default">
+                            <div class="box_1"></div>
+                            <div class="box_2"></div>
+                            <span style="background-image:url(<?=wp_get_attachment_image_url(127,'full')?>)">
+                                
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="user-select: none; width: 100%; height: 100%; display: inline-block; fill: var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255)); color: var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255)); flex-shrink: 0;" focusable="false" color="var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255))"><g color="var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255))" weight="regular"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></g></svg>
+                                <b>Convert Leads Into Customers
+                                </b>
+                            </span>
+                        </a>
+                </div>
+
+                </div>
+            </li>
+            <li>
+                <h3>Retarget and Recover Lost Leads</h3>
+                <p>Bring back visitors who didn’t convert the first time and turn interest into action.</p>
+            </li>
+            <li>
+                <h3>Gain Instant Visibility on Competitive Keywords</h3>
+                <p>Appear above competitors in search results without months of SEO work.</p>
+            </li>
+            <li>
+                <h3>Test and Scale Quickly</h3>
+                <p>Launch fast, validate ideas, and scale campaigns based on real-time performance data.</p>
+            </li>
+
+            </ul>   
+        </div>
+
+
 
     </div>
 
 </div>
 
-<div class="Values">
-    <div class="bg_overlay" style="background-image:url(http://localhost/HDM-AE/wp-content/uploads/2025/07/ChatGPT-Image-Jun-26-2025-05_38_29-PM2.png);"></div>
-    <div class="container">
 
-            <div class="title_box">
-                <h2 style="background-image:url(http://localhost/HDM-AE/wp-content/uploads/2025/05/cta3.jpg)" class="title">Turns Clicks Into Customers with PPC Advertising</h2>
-                <p class="description">Convert visitors into real customers and boost your ROI with high-performance PPC advertising.</p>
-                <a href="#" class="CTA_Default">
-                    <div class="box_1"></div>
-                    <div class="box_2"></div>
-                    <span style="background-image:url(<?=wp_get_attachment_image_url(127,'full')?>)">
-                        
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" style="user-select: none; width: 100%; height: 100%; display: inline-block; fill: var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255)); color: var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255)); flex-shrink: 0;" focusable="false" color="var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255))"><g color="var(--token-a85af9cb-7834-4006-a277-2dd1295ae376, rgb(255, 255, 255))" weight="regular"><path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path></g></svg>
-                        <b>Convert Leads Into Customers
-                        </b>
-                    </span>
-                </a>
-            </div>
-
-            <ul class="list_of_values">
-
-                    <li>
-                        <h3>Drive Targeted Traffic</h3>
-                        <p> Reach people actively searching for your products or services.</p>
-                    </li>
-                    <li class="clear_box"></li>
-                    <li>
-                        <h3>Maximize Conversion</h3>
-                        <p> Convert visitors into real customers by optimizing every click and landing page.</p>
-                    </li>
-                    <li>
-                        <h3>Achieve Better ROI</h3>
-                        <p>Get faster results and pay only for clicks that bring real value.</p>
-                    </li>
-                    <li class="clear_box"></li>
-                    <li>
-                        <h3>Retarget and Recover Lost Leads</h3>
-                        <p>Bring back visitors who didn’t convert the first time and turn interest into action.</p>
-                    </li>
-                    <li>
-                        <h3>Gain Instant Visibility on Competitive Keywords</h3>
-                        <p>Appear above competitors in search results without months of SEO work.</p>
-                    </li>
-                    <li class="clear_box"></li>
-                    <li>
-                        <h3>Test and Scale Quickly</h3>
-                        <p>Launch fast, validate ideas, and scale campaigns based on real-time performance data.</p>
-                    </li>
-
-            </ul>
-    </div>
-</div>
 
 <?php
 get_footer();
--- src/js/pages/ppc.js
+++ src/js/pages/ppc.js
@@ -7,18 +7,49 @@ setInterval(function(){
 
 
 
-var values = jQuery('.Values').offset().top;
-var ScrolledRight = 0;
-var lastScrollTop = 0;
-jQuery(document).scroll(function(e) {
-    var st = jQuery(this).scrollTop();
-    var scrolled = jQuery(this).scrollTop();
-  console.log(values);
-    if(st > values){
-        jQuery(".Values .title_box").addClass("active");
-    }else{
-        jQuery(".Values .title_box").removeClass("active");
-    }
+
+
+jQuery(document).ready(function () {
+  var $dashboard = jQuery(".ListOfValue");
+
+  jQuery(window).on("scroll", function () {
+      var sectionOffset = jQuery(".section-creative").offset().top;
+      var scrollTop = jQuery(window).scrollTop();
+      var distance = scrollTop - sectionOffset;
+
+      if (distance < 0) return;
+
+      var maxScroll = 300;
+      var progress = Math.min(distance / maxScroll, 1);
+      var rotateX = 35 * (1 - progress);
+      var opacity = 1 + 0.5 * progress;
+
+      jQuery(".ListOfValue").css({
+          "transform": "perspective(1200px) rotateX(" + rotateX + "deg) rotateY(0)",
+      });
+    
+
+     
+  });
 
 });
 
+var ListOfValue = Number(jQuery(".ListOfValue").offset().top) - Number(400);
+var title = Number(jQuery(".value_section .title_box").offset().top) - Number(100);
+var ScrolledRight = 0;
+jQuery(document).scroll(function(e) {
+    var st = jQuery(this).scrollTop();
+    var scrolled = jQuery(this).scrollTop();
+
+    if(st > ListOfValue){
+        jQuery(".ListOfValue").addClass("active");
+    }else{
+        jQuery(".ListOfValue").removeClass("active");
+    }
+    if(st > title){
+      jQuery(".value_section .title_box").addClass("active");
+    }else{
+        jQuery(".value_section .title_box").removeClass("active");
+    }
+
+});
