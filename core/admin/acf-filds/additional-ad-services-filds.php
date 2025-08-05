0ee1dc659eac6f283b267d4374e20876c52efbbb
Mobin Mirzaei <mobin.mirzaei.1377@gmail.com>
2025-08-05 12:58:59 +0330

    ad-servicess added

--- core/admin/acf-filds/additional-ad-services-filds.php
+++ core/admin/acf-filds/additional-ad-services-filds.php
@@ -136,6 +136,13 @@ if (function_exists('acf_add_local_field_group')) :
                     'value' => 'page-app.php',
                 ],
             ],
+            [
+                [
+                    'param' => 'page_template',
+                    'operator' => '==',
+                    'value' => 'page-metaAds.php',
+                ],
+            ],
         ],
         'menu_order' => 0,
         'position' => 'normal',
