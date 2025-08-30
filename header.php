f7e49b42ebd30ddbd44e8e11df8c0babe4e5c210
Meysam Khatami <khatami.maysam@gmail.com>
2025-08-30 10:31:41 +0330

    fixed app

--- header.php
+++ header.php
@@ -8,11 +8,11 @@
         gtag('js', new Date());
     </script>
      <!--Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WLQ4DP8');</script> -->
+    })(window,document,'script','dataLayer','GTM-WLQ4DP8');</script>
      <!--End Google Tag Manager -->
      <script type="text/javascript">
     (function(c,l,a,r,i,t,y){
